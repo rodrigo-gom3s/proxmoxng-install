d42cf4308d7ddbdac84fca0d501d55d5c7be9d29
Iuri Carrasqueiro <iuri.carrasqueiro@gmail.com>
2025-06-04 15:42:00 +0100

    Add logging for database location in ProxmoxNG installer

--- proxmoxng_install.sh
+++ proxmoxng_install.sh
@@ -448,6 +448,7 @@ fqdn=\"<fqdn>\" # Ex: domain.tld" >/etc/proxmoxng/middleware/example.auto_config
     fi
 
     db=$(get_toml_value "$filepath" "database" "uri")
+    echo "[INFO] - ProxmoxNG - Database location: $db"
     ls "$DB" >/dev/null 2>/dev/null
     if [ $? -ne 0 ]; then
         echo "[ERROR] - The database location is invalid or the directory does not exist."
